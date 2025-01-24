6626acfe8a1c08aeecc9aa5340de94d2696cbbf8
valentinfcarlomagnoat.gmail.com <valentinfcarlomagnoat.gmail.com>
2025-01-24 17:00:56 -0300

    Prepare build

--- backend/dist/middlewares/notFoundHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = notFoundHandler;
-const models_1 = require("../interfaces/models");
-function notFoundHandler(req, res, next) {
-    const error = new models_1.CountryError('Url not found, check request method', 404);
-    return next(error);
-}
